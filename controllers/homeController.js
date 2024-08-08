1dfab818c3a0cf4090fd3de58ff6c760c3092d93
Joao Henrique de Oiveira <joaojho7@gmail.com>
2024-08-08 10:39:01 -0300

    :sparkles: Feat: Adicionado Músicas Recomendadas

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -11,8 +11,28 @@ exports.home = async (req, res) => {
             }
         });
 
+        const genre_seeds = await axios.get('https://api.spotify.com/v1/recommendations/available-genre-seeds', {
+            headers: {
+                'Authorization': `Bearer ${req.session.accessToken}`
+            }
+        });
+
+        const recomendPlaylist = await axios.get(`https://api.spotify.com/v1/recommendations?seed_genres=${genre_seeds}`, {
+            headers: {
+                'Authorization': `Bearer ${req.session.accessToken}`
+            }
+        });
+
+        const recentTracks = await axios.get('https://api.spotify.com/v1/me/player/recently-played?limit=3', {
+            headers: {
+                'Authorization': `Bearer ${req.session.accessToken}`
+            }
+        });
+
         res.render('home', { 
             data: userData.data,
+            playlist: recomendPlaylist.data,
+            recentTracks: recentTracks.data
         });
     } catch (error) {
         res.render('error', {
--- controllers/profileController.js
+++ controllers/profileController.js
@@ -3,15 +3,28 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-exports.tops = async (req, res) => {
-    const accessToken = req.session.accessToken;
+exports.topArtists = async (req, res) => {
     await axios.get('https://api.spotify.com/v1/me/top/artists', {
         headers: {
             'Authorization': `Bearer ${accessToken}`
         }
     })
     .then(response => {
-        res.send(response.data);
+        res.send(response.data); //FRONT-END
+    })
+    .catch(error => {
+        res.send(error);
+    });
+}
+
+exports.topTracks = async (req, res) => {
+    await axios.get('https://api.spotify.com/v1/me/top/tracks', {
+        headers: {
+            'Authorization' : `Bearer ${req.session.accessToken}`
+        }
+    })
+    .then(response => {
+        res.send(response.data); //FRONT-END
     })
     .catch(error => {
         res.send(error);
