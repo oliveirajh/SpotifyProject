ff71ece2ce6a4b01cfe70b0166b3f5e65bc6c1df
Joao Victor Ferrari De Melo <joaoferraridev@gmail.com>
2024-08-08 13:59:44 -0300

    ♻️refactor: realizado tratamento de erros

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -4,29 +4,33 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 exports.topArtists = async (req, res) => {
-    await axios.get('https://api.spotify.com/v1/me/top/artists', {
-        headers: {
-            'Authorization': `Bearer ${accessToken}`
-        }
-    })
-    .then(response => {
-        res.send(response.data); //FRONT-END
-    })
-    .catch(error => {
-        res.send(error);
-    });
+    const accessToken = req.session.accessToken; // Supondo que o token esteja na sessão
+
+    try {
+        const response = await axios.get('https://api.spotify.com/v1/me/top/artists', {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+        res.send(response.data); // FRONT-END
+    } catch (error) {
+        console.error('Error fetching top artists:', error.message);
+        res.status(error.response ? error.response.status : 500).render('error', { message: error.message });
+    }
 }
 
 exports.topTracks = async (req, res) => {
-    await axios.get('https://api.spotify.com/v1/me/top/tracks', {
-        headers: {
-            'Authorization' : `Bearer ${req.session.accessToken}`
-        }
-    })
-    .then(response => {
-        res.send(response.data); //FRONT-END
-    })
-    .catch(error => {
-        res.send(error);
-    });
+    const accessToken = req.session.accessToken; // Supondo que o token esteja na sessão
+
+    try {
+        const response = await axios.get('https://api.spotify.com/v1/me/top/tracks', {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+        res.send(response.data); // FRONT-END
+    } catch (error) {
+        console.error('Error fetching top tracks:', error.message);
+        res.status(error.response ? error.response.status : 500).render('error', { message: error.message });
+    }
 }
