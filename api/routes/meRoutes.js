86e524f8496389b52e259f4f1d793ae97daa825e
Joao Henrique de Oiveira <joaojho7@gmail.com>
2024-08-13 15:04:13 -0300

    :sparkles: Feat: Adicionado Função para pegar músicas recentes na API

--- api/routes/meRoutes.js
+++ api/routes/meRoutes.js
@@ -6,5 +6,6 @@ router.get("/", meController.getProfile);
 router.get('/top-artists', meController.getMyTopArtists);
 router.get('/top-tracks', meController.getMyTopTracks);
 router.get('/saved-tracks', meController.getSavedTracks);
+router.get('/recently-played', meController.getRecentlyPlayed);
 
 module.exports = router;
--- api/services/spotifyServices.js
+++ api/services/spotifyServices.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 const getHeaders = (access_token) => (
     {'Authorization': access_token}
 )
-    
+  
+//Interações com o Perfil do usuário
 
 exports.getMyProfile = async (access_token) => {
     return await axios.get("https://api.spotify.com/v1/me", {
@@ -45,6 +46,25 @@ exports.getSavedTracks = async(access_token, limit = 5, offset = 0) => {
 
     const url = `https://api.spotify.com/v1/me/tracks?${params.toString()}`;
     return await axios.get(url, {
+        headers: getHeaders(access_token)
+    })
+}
+
+exports.getRecentlyPlayed = async(access_token, limit = 5) => {
+    const params = new URLSearchParams({
+        limit: limit.toString()
+    })
+
+    const url = `https://api.spotify.com/v1/me/player/recently-played?${params.toString()}`;
+    return await axios.get(url, {
+        headers: getHeaders(access_token)
+    })
+}
+
+//Interações com o Player
+
+exports.getCurrentTrackPlaying = async(access_token) => {
+    return await axios.get(`https://api.spotify.com/v1/me/player/currently-playing`, {
         headers: getHeaders(access_token)
     })
 }
