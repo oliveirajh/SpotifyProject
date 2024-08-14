41bd71a7989c7355b88a3601e7e44a865917ea03
Joao Victor Ferrari De Melo <joaoferraridev@gmail.com>
2024-08-14 10:44:22 -0300

    :sparkles: feat: Adiciona perfil do artista

--- APP/controllers/homeController.js
+++ APP/controllers/homeController.js
@@ -84,6 +84,42 @@ exports.search = async (req, res) => {
         //res.render('search', { data: search.data, search: req.params.track, error: req.query.error });
     }
 }
+exports.artistProfile = async (req, res) => {
+    try {
+        const artistId = req.params.id;
+        const accessToken = req.session.accessToken;
+
+        // Buscar informações do artista
+        const artist = await axios.get(`${API_URL}/artists/${artistId}`, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+
+        // Buscar os álbuns do artista
+        const albums = await axios.get(`${API_URL}/artists/${artistId}/albums`, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+
+        // Buscar as faixas mais populares do artista
+        const topTracks = await axios.get(`${API_URL}/artists/${artistId}/top-tracks?market=US`, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+
+        res.render('artistProfile', {
+            artist: artist.data,
+            albums: albums.data.items,
+            topTracks: topTracks.data.tracks
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error fetching artist profile");
+    }
+};
 
 exports.getCurrentTrack = async (req, res) => {
     try {
