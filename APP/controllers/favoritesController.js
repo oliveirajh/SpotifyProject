70edc9c274588f303fc287b9b5a8f84dd9967a38
Giovanna Gutierrez <g.gutierrez2303@gmail.com>
2024-08-15 14:38:27 -0300

    :sparkles: Feat: Adiciona controller favoritos

---
+++ APP/controllers/favoritesController.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+const API_URL = process.env.API_URL;
+
+exports.getFavorites = async (req, res) => {
+    try {
+        const { accessToken } = req.session;
+
+        const userData = await axios.get(`${API_URL}/me`, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+
+        const favoriteArtists = await axios.get(`${API_URL}/me/top-artists?limit=4`, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+
+        const favoriteSongs = await axios.get(`${API_URL}/me/top-tracks?limit=4`, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });
+
+        //Rota pendente//
+        /*const favoritePlaylists = await axios.get(`${API_URL}/users/${userData.data.id}/playlists?limit=4`, {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            }
+        });*/
+
+        res.render('favorites',{
+            user: userData.data,
+            artists: favoriteArtists.data,
+            songs: favoriteSongs.data.items,
+            /*playlists: favoritePlaylists.data,*/
+        })
+    } catch (err) {
+        res.send(err);
+    };
+};
