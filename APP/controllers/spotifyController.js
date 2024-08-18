7f6472c9a249027e0bc3a2ec101908cf5de7b27b
root <root@ip-10-0-6-51.ec2.internal>
2024-08-18 23:28:04 +0000

    :bug: Fix: Arrumando Roteamento

--- APP/controllers/spotifyController.js
+++ APP/controllers/spotifyController.js
@@ -2,8 +2,9 @@ const axios = require('axios');
 const dotenv = require('dotenv');
 
 dotenv.config();
+const API_URL = process.env.API_URL
 
 exports.auth = async (req, res) => {
-    res.redirect('http://localhost:3000/auth');
+    res.redirect(`${API_URL}/auth`);
 };
 
