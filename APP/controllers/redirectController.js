8d3fbe7d5d37d5a9c107abaa28cb21ee0aaede18
root <root@ip-10-0-6-51.ec2.internal>
2024-08-18 23:44:27 +0000

    :sparkles: Feat: Mostrando AcessToken no terminal da API

--- APP/controllers/redirectController.js
+++ APP/controllers/redirectController.js
@@ -2,5 +2,6 @@ exports.redirect = (req, res) => {
   req.session.accessToken = req.query.access_token;
   req.session.refreshToken = req.query.refresh_token;
   req.session.tokenExpiry = Date.now() + 3600 * 1000;
+  console.log(req.session.accessToken);
   res.render('redirect');
 };
